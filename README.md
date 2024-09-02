44e640d2af0db892d2f2806848b46e2f7b1f0c56
Antônio Thiago Neto <atn.coding@gmail.com>
2024-09-02 18:19:23 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# analise-olimpiada
+ Projeto para análise de resultados da Olimpíada usando Spring Boot
---
+++ src/main/java/com/example/analise_olimpiada/AnaliseDeResultadosDaOlimpiadaApplication.java
@@ -0,0 +1,13 @@
+package com.example.analise_olimpiada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnaliseDeResultadosDaOlimpiadaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnaliseDeResultadosDaOlimpiadaApplication.class, args);
+	}
+
+}

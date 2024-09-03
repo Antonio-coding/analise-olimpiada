5804d6abd9c1a81f82a12927cd2ddb23f12c6e77
Antônio Thiago Neto <112974950+Antonio-coding@users.noreply.github.com>
2024-09-03 10:25:44 -0300

    structor

--- src/main/java/com/example/analise_olimpiada/AnaliseDeResultadosDaOlimpiadaApplication.java
+++ src/main/java/com/example/analise_olimpiada/AnaliseDeResultadosDaOlimpiadaApplication.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AnaliseDeResultadosDaOlimpiadaApplication {
-  // public static void main(String[] args) {
-  // 	SpringApplication.run(AnaliseDeResultadosDaOlimpiadaApplication.class, args);
-  // }
+  public static void main(String[] args) {
+  	SpringApplication.run(AnaliseDeResultadosDaOlimpiadaApplication.class, args);
+  }
 
 }
